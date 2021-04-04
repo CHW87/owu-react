4620ceba5f068edf569130782ee1d1f2765960f5
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-04-04 22:36:23 +0300

    add HW4_inputs branch

--- src/App.js
+++ src/App.js
@@ -1,183 +1,202 @@
 import React, {useState, useEffect} from "react";
 import './App.css';
 
-const Tabs = ({tabs, selectedTab}) => {
-    return (
-        <div style={{display: "flex",}}>
-            {tabs.map(tab =>
-                <button
-                    key={tab.title}
-                    style={{
-                        flex: 1,
-                        height: '50px',
-                        background: selectedTab === tab.title ? 'orange' : 'lightgrey'
-                    }}
-                    onClick={tab.clickHandler}>{tab.title}
-                </button>)}
-        </div>
-    )
-}
+//-------------------UNCONTROLED INPUTS---------------//
 
-const PostList = ({posts}) => {
-    return (
-        <>
-            {posts.map(post => (
-                <div key ={post.id}>
-                    <h3>{post.title}</h3>
-                    <p>{post.body}</p>
-                </div>
-            ))}
-        </>
-
-    )
-}
-
-const CommentList = ({comments}) => {
-    return (
-        <>
-            {comments.map(comment => (
-                <div key={comment.id}>
-                    <h3>{comment.name}</h3>
-                    <p>{comment.body}</p>
-                </div>
-            ))}
-        </>
-
-    )
-}
-
-const AlbumList = ({albums}) => {
-    return (
-        <>
-            {albums.map(album => (
-                <div key = {album.id}>
-                    <h3>{album.title}</h3>
-                </div>
-            ))}
-        </>
-
-    )
-}
-
-const PhotoList = ({photos}) => {
-    return (
-        <>
-            {photos.map(photo => (
-                <div key={photo.id}>
-                    <h3>{photo.title}</h3>
-                    <img src={photos.thumbnailUrl}></img>
-                </div>
-            ))}
-        </>
-
-    )
-}
-
-const TodoList = ({todos}) => {
-    return (
-        <>
-            {todos.map(todo => (
-                <div key={todo.id}>
-                    <h3>{todo.title}</h3>
-                    <p>{todo.completed.toString()}</p>
-                </div>
-            ))}
-        </>
-
-    )
-}
-
-const UserList = ({users}) => {
-    return (
-        <>
-            {users.map(user => (
-                <>
-                    <h3>{user.name}--{user.username}--{user.email}</h3>
-                   
-                </>
-            ))}
-        </>
-
-    )
-}
-
-
-const urlBuilder = (resource) => `https://jsonplaceholder.typicode.com/${resource}`
-
-function App() {
-    const onTabChangeHandler = (tab) =>{
-        if(tab !== selectedTab) {
-            setSelectedTab(tab);
-            setList([])
-        }
-    }
-    
-    const tabs = [
-        {title: 'posts', clickHandler: () => onTabChangeHandler('posts')},
-        {title: 'comments', clickHandler: () => onTabChangeHandler('comments')},
-        {title: 'albums', clickHandler: () => onTabChangeHandler('albums')},
-        {title: 'photos', clickHandler: () => onTabChangeHandler('photos')},
-        {title: 'todos', clickHandler: () => onTabChangeHandler('todos')},
-        {title: 'users', clickHandler: () => onTabChangeHandler('users')},
-
-    ]
-    const [selectedTab, setSelectedTab] = useState(tabs[0].title)
-    const [list,setList] = useState([])
-
-    const fetchData = async () => {
-        const response = await fetch(urlBuilder(selectedTab));
-        const data = await response.json()
-        setList(data)
-        console.log(selectedTab, data);
-    }
-
-    useEffect(() => {
-            fetchData();
-        },
-        [selectedTab])
-
-    return (
-        <div className="App">
-            <Tabs tabs={tabs} selectedTab={selectedTab}/>
-
-            {selectedTab=== 'posts' && <PostList posts={list}/>}
-            {selectedTab=== 'comments' && <CommentList comments={list}/>}
-            {selectedTab=== 'albums' && <AlbumList albums={list}/>}
-            {selectedTab=== 'photos' && <PhotoList photos={list}/>}
-            {selectedTab=== 'todos' && <TodoList todos={list}/>}
-            {selectedTab=== 'users' && <UserList users={list}/>}
-        </div>
-    )
-}
-
-export default App
-
-// import React from 'react';
+// function App() {
+//     const firstName = React.useRef()
+//     const lastName = React.useRef()
+//     const email = React.useRef()
+//     const age = React.useRef()
+//     const password = React.useRef()
+//     const form = React.useRef()
+//     const onSubmit = (e) => {
+//         e.preventDefault();
+//         console.log(e);
+//         console.log({firstName})
+//         // firstName.current.value = ''
+//         // firstName.current.focus()
+//         // firstName.current.style.background = 'red'
 //
-// function App(props) {
+//         // const {
+//         // target: [
+//         //     {value: firstName},
+//         //     {value: lastName},
+//         //     {value: email},
+//         //     {value: age},
+//         //     {value: password}
+//         // ]
+//
+//         //         target: {
+//         //             elements: {
+//         //                 firstName,
+//         //                 lastName,
+//         //                 email,
+//         //                 age,
+//         //                 password
+//         //             }
+//         //         }
+//         //     } = e;
+//         //     console.log({firstName, lastName, email, age, password})
+//        
+//         alert(JSON.stringify(
+//             {
+//                 firstName: firstName.current.value,
+//                 lastName: lastName.current.value,
+//                 email: email.current.value,
+//                 age: age.current.value,
+//                 password: password.current.value
+//             }, null, 2))
+//        
+//         //reset for clear form//
+//         form.current.reset()
+//        
+//         //reset for clear inputs//
+//         // firstName.current.value = '';
+//         // lastName.current.value = '';
+//         // email.current.value = '';
+//         // age.current.value = '';
+//         // password.current.value = '';
+//     }
+//
+//
 //     return (
-//         <div></div>
+//         <form 
+//             ref = {form}
+//             onSubmit={onSubmit}>
+//             <div className="App">
+//                 <h1>this is input</h1>
+//                 <br/>
+//                 <br/>
+//                 <input ref={firstName} type="text" name='firstName' placeholder="enter your firstname"/>
+//                 <br/>
+//                 <br/>
+//                 <input ref={lastName} type="text" name='lastName' placeholder="enter your lastname"/>
+//                 <br/>
+//                 <br/>
+//                 <input ref={email} type="email" name='email' placeholder="enter your email"/>
+//                 <br/>
+//                 <br/>
+//                 <input ref={age} type="number" name='age' placeholder="enter your age"/>
+//                 <br/>
+//                 <br/>
+//                 <input ref={password} type="password" name='password' placeholder="enter your pasword"/>
+//                 <br/>
+//                 <br/>
+//                 <button type='submit'>Submit</button>
+//             </div>
+//         </form>
 //     );
 // }
 //
-// export default App;
+//export default App;
+
+//-------------------CONTROLED INPUTS------------------//
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
+const AVAILABLE_RESOURCES = [
+    'posts',
+    'comments',
+    'albums',
+    'photos',
+    'todos',
+    'users',
+]
+
+function App() {
+    const [endpointFields, setEndpointFields] = useState({
+        endPoint: '',
+        id: ''
+    })
+    const {endpoint, id} = endpointFields;
+    // const [endPoint, setEndpoint] = useState('')
+    // const [id, setId] = useState('')
+    const [errorMassage, setErrorMassage] = useState('')
 
 
-// урл
-// https://jsonplaceholder.typicode.com/
-//
-// ендпоінти
-// /posts 
-// /comments 
-// /albums 
-// /photos 
-// /todos 
-// /users 
-//
-//
-// потрібно створити логіку, яка задовільнить наступні вимоги
-// в нас має бути 6 кнопок, які дозволяють нам переключатись між 'табами' (posts, comments, albums, photos, todos, users)
-// дефолтно обрана таба- пости
-// по кліку на кнопку ми повинні підтягнути відповідний список і зрендерити його через .map
-// лише 1 список видимий одночасно
-// потрібно створити 6 компонент, які займатимуться рендерінгом списків (отримуватимуть пропсами список)- PostList, CommentsList...
+    const [items, setItems] = useState([])
+    const [singleItem, setSingleItem] = useState(null)
+
+    const validateEndpoint = () => {
+        let isOk = true;
+        if (!endpoint) {
+            setErrorMassage('first input is required!!!')
+            return false;
+        } else if (!AVAILABLE_RESOURCES.includes(endpoint.trim().toLowerCase())) {
+            setErrorMassage('value is not valid,try to use smth from this list : posts, comments, albums, photos, todos, users')
+            return false;
+        }
+        return true
+    }
+    const validateId = () => {
+        let isOk = true;
+
+        const idToNum = Number(id)
+
+        if (!idToNum && id !== '' && idToNum !== 0) {
+            setErrorMassage('value of second input is not valid,pls use numeric value')
+            return false;
+        } else if ((idToNum < 1 || idToNum > 100) && id !== '') {
+            setErrorMassage('value of second input is out of range,pls use 1-100')
+            return false;
+        }
+        ;
+        return true;
+    }
+    
+    const resetError = () => setErrorMassage('') 
+
+
+    const onSubmit = () => {
+        const isEndpointValid = validateEndpoint();
+        const isIdOk = validateId();
+        if (isIdOk && isEndpointValid) {
+            fetchData();
+            resetError();
+        }
+    }
+
+    const fetchData = async () => {
+        const response = await fetch(`${BASE_URL}/${endpoint.trim().toLowerCase()}/${id.trim()}`)
+        const json = await response.json()
+
+        if (id) {
+            setSingleItem(json)
+            setItems([])
+            return
+        }
+        setSingleItem(null)
+        setItems(json)
+    }
+    const onFieldUpdate = ({target: {name, value}}) => setEndpointFields({...endpointFields, [name]: value})
+
+
+    return (
+        <div className="App">
+            <input value={endpoint} onChange={onFieldUpdate} name='endpoint' type="text"
+                   placeholder="E.g.posts,comments, todos,etc"/>
+            <br/>
+            <br/>
+            <input value={id} onChange={onFieldUpdate} name='id' type="text" placeholder="resourse id,e.g. 1,2,3"/>
+            <br/>
+            <br/>
+
+            <button onClick={onSubmit}>fetch data</button>
+            <hr/>
+            <h1 style={{color: 'red'}}>{errorMassage}</h1>
+
+            <div style={{width: '400px', textAlign: 'left', padding: '20px', whiteSpace: 'pre-wrap'}}>
+                {singleItem &&
+                JSON.stringify(singleItem, null, 2)}
+
+
+            </div>
+            <hr/>
+            <div>
+                {items.map(el => (<div>{el.id}-{el.title}-{el.name}</div>))}
+            </div>
+        </div>
+    );
+}
+
+export default App;
