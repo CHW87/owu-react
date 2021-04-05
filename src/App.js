431c992f585ecdcf276d02a5aec2deaca8dfb3b6
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-04-05 16:22:32 +0300

    add HW4_inputs branch

--- src/App.js
+++ src/App.js
@@ -1,202 +1,120 @@
-import React, {useState, useEffect} from "react";
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useParams,
+    useRouteMatch,
+    useLocation,
+    useHistory
+} from "react-router-dom";
 
-//-------------------UNCONTROLED INPUTS---------------//
-
-// function App() {
-//     const firstName = React.useRef()
-//     const lastName = React.useRef()
-//     const email = React.useRef()
-//     const age = React.useRef()
-//     const password = React.useRef()
-//     const form = React.useRef()
-//     const onSubmit = (e) => {
-//         e.preventDefault();
-//         console.log(e);
-//         console.log({firstName})
-//         // firstName.current.value = ''
-//         // firstName.current.focus()
-//         // firstName.current.style.background = 'red'
-//
-//         // const {
-//         // target: [
-//         //     {value: firstName},
-//         //     {value: lastName},
-//         //     {value: email},
-//         //     {value: age},
-//         //     {value: password}
-//         // ]
-//
-//         //         target: {
-//         //             elements: {
-//         //                 firstName,
-//         //                 lastName,
-//         //                 email,
-//         //                 age,
-//         //                 password
-//         //             }
-//         //         }
-//         //     } = e;
-//         //     console.log({firstName, lastName, email, age, password})
-//        
-//         alert(JSON.stringify(
-//             {
-//                 firstName: firstName.current.value,
-//                 lastName: lastName.current.value,
-//                 email: email.current.value,
-//                 age: age.current.value,
-//                 password: password.current.value
-//             }, null, 2))
-//        
-//         //reset for clear form//
-//         form.current.reset()
-//        
-//         //reset for clear inputs//
-//         // firstName.current.value = '';
-//         // lastName.current.value = '';
-//         // email.current.value = '';
-//         // age.current.value = '';
-//         // password.current.value = '';
-//     }
-//
-//
-//     return (
-//         <form 
-//             ref = {form}
-//             onSubmit={onSubmit}>
-//             <div className="App">
-//                 <h1>this is input</h1>
-//                 <br/>
-//                 <br/>
-//                 <input ref={firstName} type="text" name='firstName' placeholder="enter your firstname"/>
-//                 <br/>
-//                 <br/>
-//                 <input ref={lastName} type="text" name='lastName' placeholder="enter your lastname"/>
-//                 <br/>
-//                 <br/>
-//                 <input ref={email} type="email" name='email' placeholder="enter your email"/>
-//                 <br/>
-//                 <br/>
-//                 <input ref={age} type="number" name='age' placeholder="enter your age"/>
-//                 <br/>
-//                 <br/>
-//                 <input ref={password} type="password" name='password' placeholder="enter your pasword"/>
-//                 <br/>
-//                 <br/>
-//                 <button type='submit'>Submit</button>
-//             </div>
-//         </form>
-//     );
-// }
-//
-//export default App;
-
-//-------------------CONTROLED INPUTS------------------//
-
-const BASE_URL = 'https://jsonplaceholder.typicode.com'
-const AVAILABLE_RESOURCES = [
-    'posts',
-    'comments',
-    'albums',
-    'photos',
-    'todos',
-    'users',
-]
-
-function App() {
-    const [endpointFields, setEndpointFields] = useState({
-        endPoint: '',
-        id: ''
-    })
-    const {endpoint, id} = endpointFields;
-    // const [endPoint, setEndpoint] = useState('')
-    // const [id, setId] = useState('')
-    const [errorMassage, setErrorMassage] = useState('')
-
-
-    const [items, setItems] = useState([])
-    const [singleItem, setSingleItem] = useState(null)
-
-    const validateEndpoint = () => {
-        let isOk = true;
-        if (!endpoint) {
-            setErrorMassage('first input is required!!!')
-            return false;
-        } else if (!AVAILABLE_RESOURCES.includes(endpoint.trim().toLowerCase())) {
-            setErrorMassage('value is not valid,try to use smth from this list : posts, comments, albums, photos, todos, users')
-            return false;
-        }
-        return true
-    }
-    const validateId = () => {
-        let isOk = true;
-
-        const idToNum = Number(id)
-
-        if (!idToNum && id !== '' && idToNum !== 0) {
-            setErrorMassage('value of second input is not valid,pls use numeric value')
-            return false;
-        } else if ((idToNum < 1 || idToNum > 100) && id !== '') {
-            setErrorMassage('value of second input is out of range,pls use 1-100')
-            return false;
-        }
-        ;
-        return true;
-    }
-    
-    const resetError = () => setErrorMassage('') 
-
-
-    const onSubmit = () => {
-        const isEndpointValid = validateEndpoint();
-        const isIdOk = validateId();
-        if (isIdOk && isEndpointValid) {
-            fetchData();
-            resetError();
-        }
-    }
-
-    const fetchData = async () => {
-        const response = await fetch(`${BASE_URL}/${endpoint.trim().toLowerCase()}/${id.trim()}`)
-        const json = await response.json()
-
-        if (id) {
-            setSingleItem(json)
-            setItems([])
-            return
-        }
-        setSingleItem(null)
-        setItems(json)
-    }
-    const onFieldUpdate = ({target: {name, value}}) => setEndpointFields({...endpointFields, [name]: value})
-
-
+export default function App() {
     return (
-        <div className="App">
-            <input value={endpoint} onChange={onFieldUpdate} name='endpoint' type="text"
-                   placeholder="E.g.posts,comments, todos,etc"/>
-            <br/>
-            <br/>
-            <input value={id} onChange={onFieldUpdate} name='id' type="text" placeholder="resourse id,e.g. 1,2,3"/>
-            <br/>
-            <br/>
-
-            <button onClick={onSubmit}>fetch data</button>
-            <hr/>
-            <h1 style={{color: 'red'}}>{errorMassage}</h1>
-
-            <div style={{width: '400px', textAlign: 'left', padding: '20px', whiteSpace: 'pre-wrap'}}>
-                {singleItem &&
-                JSON.stringify(singleItem, null, 2)}
-
-
-            </div>
-            <hr/>
+        <Router>
             <div>
-                {items.map(el => (<div>{el.id}-{el.title}-{el.name}</div>))}
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/posts">Posts</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/" component={Home} exact/>
+                    
+                    <Route path="/posts">
+                        <Posts/>
+                    </Route>
+
+                    {/*<Route path="/posts/:id">*/}
+                    {/*    <PostDetails/>*/}
+                    {/*</Route>*/}
+
+                    {/*<Route path="/users" render={(args) => {*/}
+                    {/*    console.log(args);*/}
+                    {/*    return <Users/>*/}
+                    {/*}}>*/}
+
+                    {/* <Route>*/}
+                    {/*    <Redirect to="/"/>*/}
+                    {/*</Route>*/}
+
+                    <Route>
+                        <h1>PAGE NOT FOUND</h1>
+                    </Route>
+                </Switch>
             </div>
-        </div>
+        </Router>
     );
 }
 
-export default App;
+function Home(props) {
+    console.log(props);
+    return <h2>Home</h2>;
+}
+
+function Posts(props) {
+    const [posts, setPosts] = React.useState([])
+    const fetchData = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const json = await response.json();
+
+        setPosts(json)
+    }
+    React.useEffect(() => {
+        fetchData();
+    }, [])
+
+    return (<div>
+            <Switch>
+                <Route path="/posts/:id" exact>
+                    <PostDetails/>
+                </Route>
+                <Route>
+                    <Redirect to="/posts /"/>
+                </Route>
+
+            </Switch>
+            <ul>
+                {posts.map(el => <Link to={`/posts/${el.id}`}>
+                    <li>{el.title}-{el.id}</li>
+                </Link>)}
+            </ul>
+        </div>
+    )
+}
+
+function PostDetails(props) {
+    const match = useRouteMatch();
+    const {id} = useParams();
+    const location = useLocation();
+    const history = useHistory();
+    // console.log({match, params, location});
+
+    const [post, setPost] = React.useState()
+    const fetchData = async () => {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        const json = await response.json();
+
+        setPost(json)
+    }
+    React.useEffect(() => {
+        fetchData();
+    }, [id])
+
+    return (<div>
+            <h1>Post Details</h1>
+            {post && (<><h3>{post.title}</h3>-<p>{post.body}</p></>)}
+            <button onClick={() => history.push(`/posts/${+id + 1}`)}>go to next post</button>
+        </div>
+    )
+}
